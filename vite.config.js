196be554047368929938ca420aecabca9d6c88bb
Dethie DIOUF <dethiediouf144165@gmail.com>
2026-02-12 19:31:45 +0000

    update fav icon

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5001',
+        target: 'https://samaterrainbackend.onrender.com',
         changeOrigin: true,
       },
     },
